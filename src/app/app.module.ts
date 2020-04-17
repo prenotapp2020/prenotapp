0c52fe82955662e4d731fb649272a6e5f569cd8d
Alessandro Vertecchi <alessandro.vertecchi@eng.it>
2020-04-17 16:04:43 +0200

    risolta anomalia ionic serve - tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import firebaseConfig from './firebase'
+//import firebaseConfig from './firebase'
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(),  AngularFireModule.initializeApp(firebaseConfig),AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),  
+    //AngularFireModule.initializeApp(firebaseConfig),
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
